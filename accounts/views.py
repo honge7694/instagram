e2056dc988fc5e7bfef28a990f6e59c72f958df1
hongui <honge7694@naver.com>
2022-06-16 00:57:43 +0900

    로그인/로그아웃 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+
+
+login = LoginView.as_view(template_name="accounts/login_form.html")
+
+
+def logout(request): 
+    messages.success(request, '로그아웃 되었습니다.')
+    return logout_then_login(request)
 
 
 def signup(request):
@@ -8,11 +18,12 @@ def signup(request):
         form = SignupForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            signed_user = form.save()
+            auth_login(request, signed_user)
             messages.success(request, "회원가입을 환영합니다.")
             next_url = request.GET.get('next', '/')
 
-            return redirect("root")
+            return redirect(next_url)
     else:
         form = SignupForm()
     
--- config/settings/common.py
+++ config/settings/common.py
@@ -130,3 +130,5 @@ INTERNAL_IPS = ['127.0.0.1']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
